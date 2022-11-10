815ecca3395ee8125af207c72cdfcd87a330ba86
zhanghh <1423044909@qq.com>
2022-11-10 20:36:54 +0800

    1

--- zh-github-stu/src/main/java/com/example/zhgithubstu/ZhGithubStuApplication.java
+++ zh-github-stu/src/main/java/com/example/zhgithubstu/ZhGithubStuApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZhGithubStuApplication {
 
     public static void main(String[] args) {
+        /**
+         * 启动类
+         */
         SpringApplication.run(ZhGithubStuApplication.class, args);
     }
 
