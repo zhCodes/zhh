be6350b35e7c60ef32e922fa03d6d6f397839f3b
zhanghh <1423044909@qq.com>
2022-11-10 20:35:53 +0800

    1

---
+++ zh-github-stu/src/main/java/com/example/zhgithubstu/ZhGithubStuApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhgithubstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhGithubStuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhGithubStuApplication.class, args);
+    }
+
+}
---
+++ zh-github-stu/src/test/java/com/example/zhgithubstu/ZhGithubStuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zhgithubstu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhGithubStuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
